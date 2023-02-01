ece435f85832f14bed84dfc8fed3174c8c2b7de6
sumitradas94 <sumitradas94@gmail.com>
2023-02-01 17:44:21 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeePayroll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            EmployeeModel employeeModel = new EmployeeModel();
+
+            employeeModel.EmployeeName = "Rita";
+            employeeModel.PhoneNumber = "9898989889";
+            employeeModel.Address = "13 street";
+            employeeModel.Department = "Hr";
+            employeeModel.Gender = 'F';
+            employeeModel.BasicPay = 20000;
+            employeeModel.Deduction = 200;
+            employeeModel.TaxablePay = 2500;
+            employeeModel.Tax = 1000;
+            employeeModel.NetPay = 200;
+            employeeModel.City = "Banglore";
+            employeeModel.Country = "India";
+
+            //employeeRepo.AddEmployee(employeeModel);
+            employeeRepo.GetAllEmployee();
+        }
+    }
+}
